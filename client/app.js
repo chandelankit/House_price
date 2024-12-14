38b6c42c128488897132f8603338c12830470e3a
Ankit Singh Chandel <ankitlucky1016@gmail.com>
2024-12-14 21:13:37 +0530

    first commit

---
+++ client/app.js
@@ -0,0 +1,108 @@
+var globalPrice;
+var conversionRate;
+var selectedCurr;
+var dollarRate;
+
+function getBathValue() {
+    var uiBathrooms = document.getElementsByName("uiBathrooms");
+    for(var i in uiBathrooms) {
+      if(uiBathrooms[i].checked) {
+          return parseInt(i)+1;
+      }
+    }
+    return -1; // Invalid Value
+  }
+  
+  function getBHKValue() {
+    var uiBHK = document.getElementsByName("uiBHK");
+    for(var i in uiBHK) {
+      if(uiBHK[i].checked) {
+          return parseInt(i)+1;
+      }
+    }
+    return -1; // Invalid Value
+  }
+  
+  function onClickedEstimatePrice() {
+    console.log("Estimate price button clicked");
+    var sqft = document.getElementById("uiSqft");
+    var bhk = getBHKValue();
+    var bathrooms = getBathValue();
+    var location = document.getElementById("uiLocations");
+
+    var url = "http://127.0.0.1:5000/predict_home_price";
+
+    return $.post(url, {
+        total_sqft: parseFloat(sqft.value),
+        bhk: bhk,
+        bath: bathrooms,
+        location: location.value
+    }).then(function(data) {
+        globalPrice = data.estimated_price;
+        document.getElementById("uiEstimatedPrice").innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+        return data.estimated_price;
+    });
+}
+
+function fetchConversionRate() {
+    var url = "https://v6.exchangerate-api.com/v6/3209a9e3f1d4e9c87ad70e7d/latest/USD";
+    selectedCurr = document.getElementById('uiCurrency').value;
+    
+    return $.get(url).then(function(data) {
+        conversionRate = data.conversion_rates[selectedCurr]
+        dollarRate = data.conversion_rates.INR
+        return conversionRate;
+    });
+}
+
+function priceInDollars() {
+    Promise.all([onClickedEstimatePrice(), fetchConversionRate()])
+        .then(function([price, rate]) {
+            var estdollar = document.getElementById("uiDollarPrice");
+            console.log(rate)
+            console.log(dollarRate)
+            estdollar.innerHTML = "<h2>"+ selectedCurr + " "+ ((price * 100000)/dollarRate) * rate + "</h2>";
+        })
+        .catch(function(error) {
+            console.error("Error occurred:", error);
+        });
+}
+
+
+function onPageLoad() {
+    console.log( "document loaded" );
+    var url = "http://127.0.0.1:5000/get_location_names";
+    var url1 = "https://v6.exchangerate-api.com/v6/3209a9e3f1d4e9c87ad70e7d/latest/USD"
+    $.get(url,function(data, status) {
+        console.log("got response for get_location_names request");
+        if(data) {
+            var locations = data.locations;
+            var uiLocations = document.getElementById("uiLocations");
+            $('#uiLocations').empty();
+            for(var i in locations) {
+                var opt = new Option(locations[i]);
+                $('#uiLocations').append(opt);
+            }
+            var newopt = new Option("Choose A location")
+            newopt.selected = true;
+            $('#uiLocations').append(newopt);
+        }
+    });
+    $.get(url1,function(data, status) {
+      console.log(data)
+        if(data) {
+            var currencies = data.conversion_rates;
+            var keys = Object.keys(currencies)
+            $('#uiCurrency').empty();
+            for(var i in keys) {
+                var opt = new Option(keys[i]);
+                $('#uiCurrency').append(opt);
+            }
+            var newopt = new Option("Choose A Currency")
+            newopt.selected = true;
+            $('#uiCurrency').append(newopt);
+        }
+    });
+  }
+
+window.onload = onPageLoad;
---
+++ server/server.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify, send_from_directory
+import util
+
+app = Flask(__name__, static_folder="../client", template_folder="../client")
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'app.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server...")
+    util.load_saved_artifacts()
+    app.run(debug=False, host="0.0.0.0", port=5000)
